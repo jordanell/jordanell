97ff25839a404d244abaad598f682934e3a748b0
Jordan Ell <jordan.ell7@gmail.com>
2015-12-02 21:19:48 -0800

    Added featured post to home page.

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -2,5 +2,8 @@ class RootController < ApplicationController
   def show
     # Get the featured projects and limit to 3
     @featured_projects = Project.where(featured: true)[0..3]
+
+    # Get the featured post and limit to first one
+    @featured_post = Post.where(featured: true).first
   end
 end
